923d30aa31eb349a428854868780935941687db5
Albert Chen <algebraprodigy@gmail.com>
2016-04-28 15:49:29 +0800

    added create and new controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,18 @@
 class EventsController < ApplicationController
 	def index
 	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.create(event_params)
+		redirect_to event_path
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:image, :name)
+	end
 end
